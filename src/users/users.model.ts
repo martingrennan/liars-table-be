f6dc29e13ab6225f65f1c49ce117a73dbef2a78f
martingrennan <martingrennan@live.co.uk>
2025-01-15 15:03:02 +0000

    removed name field from model

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -3,7 +3,6 @@ import { ObjectId } from "mongodb";
 export default class User {
   constructor(
     public _id: string,
-    public name: string, 
     public username: string, 
     public email: string, 
     public avatar: string = "https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png", 
