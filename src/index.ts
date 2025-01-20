1923ba21cdc0b184dad64732087933545357df4b
Craig <craigjrobinson@hotmail.co.uk>
2025-01-20 14:27:29 +0000

    Added working room join logic.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { connectToDatabase } from "./services/database.service";
 import { usersRouter } from "./users/users.routes";
 import { Server } from "socket.io";
 import http from "http";
-// import { setupSockets } from "./socket";
+import { setupSockets } from "./socket";
 
 export const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -16,97 +17,19 @@ const io = new Server(server, {
   },
 });
 
-server.listen(8080, () => {
-  console.log("Server running at http://localhost:8080");
+setupSockets(io);
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
-// Database connection and routing setup
 connectToDatabase()
   .then(() => {
     app.use("/", usersRouter);
+    console.log("Database connected successfully");
   })
   .catch((error: Error) => {
     console.error("Database connection failed", error);
     process.exit();
-  })
-  .then(() => {
-    io.on("connection", (socket) => {
-      console.log("A new socket connection has been made:", socket.id);
-      // setupSockets(socket); // Initialize the socket events
-      console.log("in socket");
-      interface Room {
-        roomName: string;
-        isPrivate: boolean;
-        password: string | null;
-        playerCount: number;
-      }
-
-      let availableRooms: Room[] = [];
-
-      console.log("A user has connected");
-
-      // Event to create a new room
-      socket.on(
-        "createRoom",
-        (room: { roomName: string; password: string }, callback: Function) => {
-          console.log("Received createRoom data:", room);
-          const { roomName, password } = room;
-          const validRoom = availableRooms.find(
-            (room) => room.roomName === roomName
-          );
-          if (validRoom) {
-            console.log("Room name already exists:", roomName);
-            callback("Room name already exists");
-          } else {
-            const newRoom: Room = {
-              roomName,
-              isPrivate: password.length > 0,
-              password: password || null,
-              playerCount: 0,
-            };
-            availableRooms.push(newRoom);
-            console.log("New room created:", newRoom);
-            io.emit("activeRooms", availableRooms);
-            callback("Room created successfully");
-          }
-          console.log("Current available rooms:", availableRooms);
-        }
-      );
-
-      // Event to request active rooms
-      socket.on("requestActiveRooms", () => {
-        console.log("Active rooms requested. Current rooms:", availableRooms);
-        socket.emit("activeRooms", availableRooms);
-      });
-
-      // Event to join an existing room
-      socket.on("joinRoom", (roomName: string, password: string | null) => {
-        console.log(
-          `Join room request: roomName=${roomName}, password=${password}`
-        );
-        const validRoom = availableRooms.find(
-          (room) => room.roomName === roomName
-        );
-
-        if (validRoom) {
-          if (validRoom.password === password && validRoom.playerCount < 4) {
-            socket.join(roomName);
-            validRoom.playerCount++;
-            console.log(
-              `User joined room: ${roomName}, Updated room data:`,
-              validRoom
-            );
-          } else {
-            console.log(
-              `Join failed for room: ${roomName}. Password incorrect or room full.`
-            );
-            socket.emit("invalid room name or password or the room is full");
-          }
-        } else {
-          console.log(`Room "${roomName}" not found.`);
-          socket.emit("invalid room name or password or the room is full");
-        }
-      });
-      socket.emit("activeRooms", availableRooms);
-    });
   });
