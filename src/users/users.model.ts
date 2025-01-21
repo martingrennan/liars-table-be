89a5fece30ff17a19fe166a1e55bf2bc76126da9
martingrennan <martingrennan@live.co.uk>
2025-01-21 12:55:13 +0000

    changed user model

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,10 +1,20 @@
 import { ObjectId } from "mongodb";
 
+interface Card {
+  code: string;
+  image: string;
+  images: object;
+  suit: string;
+  value: string;
+}
+
 export default class User {
   constructor(
     public _id: string,
     public username: string, 
     public email: string, 
+    public hand: Card[],
     public avatar: string = "https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png", 
     public id?: ObjectId) {}
+
 }
